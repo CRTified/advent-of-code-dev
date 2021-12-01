1e3e601e2d6dfbcf826533d8dc5dd143a17b3613
Carl Richard Theodor Schneider <dev.github@crtified.me>
2021-12-01 15:54:55 +0100

    Solution Day 1

--- src/AOC/Challenge/Day01.hs
+++ src/AOC/Challenge/Day01.hs
@@ -22,22 +22,48 @@
 --     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day01 (
-    -- day01a
-  -- , day01b
+    day01a
+  , day01b
   ) where
 
 import           AOC.Prelude
 
-day01a :: _ :~> _
+data Change = Incr Int | Decr Int | Same
+
+mkChange :: Int -> Int -> Change
+mkChange a b
+  | a > b = Decr $ a - b
+  | a < b = Incr $ b - a
+  | otherwise = Same
+
+pairwiseList :: [a] -> [(a, a)]
+pairwiseList [] = []
+pairwiseList (_:[]) = []
+pairwiseList (x1:x2:xs) = (x1, x2) : (pairwiseList (x2:xs))
+
+day01a :: [Int] :~> Int
 day01a = MkSol
-    { sParse = Just
+    { sParse = (Just).catMaybes.(map (readMaybe) ).lines
     , sShow  = show
-    , sSolve = Just
+    , sSolve = \input -> Just $ length $ [ a | Incr a <- changeList input]
     }
+    where
+      changeList = (map (uncurry mkChange)).pairwiseList 
+
+slidingWindow :: Num a => Int -> [a] -> [a]
+slidingWindow wsize = go
+  where
+    go xs
+      | (length window) < wsize = []
+      | otherwise = (sum window) : (go $ drop 1 xs)
+      where
+        window = take wsize xs
 
 day01b :: _ :~> _
 day01b = MkSol
-    { sParse = Just
+    { sParse = (Just).catMaybes.(map (readMaybe) ).lines
     , sShow  = show
-    , sSolve = Just
+    , sSolve = \input -> Just $ length $ [ a | Incr a <- changeList input]
     }
+    where
+      changeList = (map (uncurry mkChange)).pairwiseList.(slidingWindow 3)
