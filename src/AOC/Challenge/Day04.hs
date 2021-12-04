9aa405ec9bb3a0d7f986e372c960aa9b4d3dfead
Carl Richard Theodor Schneider <dev.github@crtified.me>
2021-12-04 20:01:02 +0100

    Day 4 Solution

--- src/AOC/Challenge/Day04.hs
+++ src/AOC/Challenge/Day04.hs
@@ -1,6 +1,3 @@
-{-# OPTIONS_GHC -Wno-unused-imports   #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 -- |
 -- Module      : AOC.Challenge.Day04
 -- License     : BSD3
@@ -11,33 +8,107 @@
 -- Day 4.  See "AOC.Solver" for the types used in this module!
 --
 -- After completing the challenge, it is recommended to:
---
--- *   Replace "AOC.Prelude" imports to specific modules (with explicit
---     imports) for readability.
--- *   Remove the @-Wno-unused-imports@ and @-Wno-unused-top-binds@
---     pragmas.
--- *   Replace the partial type signatures underscores in the solution
---     types @_ :~> _@ with the actual types of inputs and outputs of the
---     solution.  You can delete the type signatures completely and GHC
---     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day04 (
-    -- day04a
-  -- , day04b
+     day04a
+   , day04b
   ) where
 
-import           AOC.Prelude
+import AOC.Solver ((:~>)(..))
+import Data.Maybe (isNothing, catMaybes)
+import Data.Void
+import Text.Read (readMaybe)
+import Text.Megaparsec
+import Text.Megaparsec.Char (char, digitChar, hspace, newline)
+import Control.Monad.State.Lazy as S
 
-day04a :: _ :~> _
+type Board = [[Maybe Int]]
+type Game = [Int]
+
+type Parser = Parsec Void String
+
+-- A row shall be 5 Ints
+rowP :: Parser [Maybe Int]
+rowP = count 5 (hspace >> readMaybe <$> some digitChar)
+
+-- A board shall be 5 rows
+boardP :: Parser Board
+boardP = count 5 (newline >> rowP)
+
+-- A game sequence shall be a list of Ints, separated by comma
+gameP :: Parser Game
+gameP = (read <$> some digitChar) `sepBy1` (char ',')
+
+-- The puzzle input is a game, and then a list of whitespace separated boards
+inputP :: Parser (Game, [Board])
+inputP = do
+  game <- gameP
+  boards <- some (newline >> boardP)
+  return (game, boards)
+ 
+-- Fetches the nth row of the board
+boardRow :: Int -> Board -> [Maybe Int]
+boardRow n b = b !! n
+
+-- Fetches the nth column of the Board
+boardCol :: Int -> Board -> [Maybe Int]
+boardCol n b = map (\r -> r !! n) b
+
+-- A board has won if one column or one row is crossed out
+hasWon :: Board -> Bool
+hasWon b = any (all isNothing) $ [boardRow x b | x <- [0..4]] ++ [boardCol x b | x <- [0..4]]
+
+-- To update a board, just keep all values and replace n with Nothing
+updateBoard :: Int -> Board -> Board
+updateBoard n = map (map replaceNum)
+  where
+    replaceNum Nothing = Nothing
+    replaceNum (Just x)
+      | n == x = Nothing
+      | otherwise = Just x
+
+-- Fetch all the remaining numbers
+boardRemaining :: Board -> [Int]
+boardRemaining = catMaybes . concat 
+
+-- The solution is the product of the winning number and the sum of remaining numbers
+getSolution1 :: Int -> Board -> Int
+getSolution1 n b = n * (sum $ boardRemaining b)
+
+-- Keep consuming the game input until one board has won
+runBingoUntilWin :: Game -> S.State [Board] (Maybe Int)
+runBingoUntilWin [] = return Nothing
+runBingoUntilWin (x:xs) = do
+  modify $ map (updateBoard x)
+  boards <- get
+  if (hasWon) `any` boards then -- We have a winner
+    return $ Just $ getSolution1 x $ head $ filter (hasWon) boards
+    else
+    runBingoUntilWin xs
+
+-- Keep consuming the game input until the last board has won
+runBingoUntilOneUnfinished :: Game -> S.State [Board] (Maybe Int)
+runBingoUntilOneUnfinished [] = return Nothing
+runBingoUntilOneUnfinished (x:xs) = do
+  -- We do not need to keep boards that already won
+  modify $ (filter (not . hasWon)) . map (updateBoard x)
+  boards <- get
+  if (length boards) == 1 then
+    runBingoUntilWin xs -- Only one board left, keep going until it wins
+    else
+    runBingoUntilOneUnfinished xs
+    
+    
+day04a :: (Game, [Board]) :~> Int
 day04a = MkSol
-    { sParse = Just
+    { sParse = parseMaybe inputP
     , sShow  = show
-    , sSolve = Just
+    , sSolve = \(game, boards) -> evalState (runBingoUntilWin game) boards
     }
-
-day04b :: _ :~> _
+    
+day04b :: (Game, [Board]) :~> Int
 day04b = MkSol
-    { sParse = Just
+    { sParse = parseMaybe inputP
     , sShow  = show
-    , sSolve = Just
+    , sSolve = \(game, boards) -> evalState (runBingoUntilOneUnfinished game) boards
     }
