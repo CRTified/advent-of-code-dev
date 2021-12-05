0a3596ce0d689f7b29337f525f0afd0abee5d02c
Carl Richard Theodor Schneider <dev.github@crtified.me>
2021-12-05 18:22:05 +0100

    Add comments

--- src/AOC/Challenge/Day05.hs
+++ src/AOC/Challenge/Day05.hs
@@ -25,6 +25,7 @@ data Line = Line Point Point
 
 type Parser = Parsec Void String
 
+-- A point are two digits separated by ,
 pointP :: Parser Point
 pointP = do
   x <- read <$> some digitChar
@@ -32,6 +33,7 @@ pointP = do
   y <- read <$> some digitChar
   return (x, y)
 
+-- A line are two points separated by ->
 lineP :: Parser Line
 lineP = do
   p1 <- pointP
@@ -40,9 +42,12 @@ lineP = do
   space
   return $ Line p1 p2
 
+-- two vectors are colinear in 2D if their scalar product is 0
 isColinear :: Point -> Point -> Bool
 isColinear (x1, y1) (x2, y2) = 0 == x1 * y2 - y1 * x2
 
+-- A point is on the line if the two endpoints of a line are colinear when
+-- shifting the origin to the point under test
 isOnLine :: Point -> Line -> Bool
 isOnLine (xp, yp) (Line (x1, y1) (x2, y2)) =
   and [ (inRange (x1, x2) xp) || (inRange (x2, x1) xp)
@@ -50,9 +55,12 @@ isOnLine (xp, yp) (Line (x1, y1) (x2, y2)) =
       , isColinear (x1 - xp, y1 - yp) (x2 - xp, y2 - yp)
       ]
 
+-- A line is straight if one of the coordinates of the endpoints is the same
 isStraight :: Line -> Bool
 isStraight (Line (x1, y1) (x2, y2)) = (x1 == x2) || (y1 == y2)
 
+-- The bounding box around the playing field is just the limit of all
+-- lines in all four directions
 getBoundingBox :: [Line] -> (Int, Int, Int, Int)
 getBoundingBox = foldr (update) (maxBound, maxBound, minBound, minBound)
   where
@@ -63,12 +71,14 @@ getBoundingBox = foldr (update) (maxBound, maxBound, minBound, minBound)
       , maximum [y1, y2, maxy]
       )
 
+-- The resulting map is just a 2D map where the multiplicity for each point is counted
 drawMap :: [Line] -> [[Int]]
 drawMap ls = map (map (countLines)) [[(x, y) | y<- [miny..maxy]] | x <- [minx..maxx]]
   where
     (minx, miny, maxx, maxy) = getBoundingBox ls
     countLines p = length $ filter (isOnLine p) ls
 
+-- Filter the lines for straight ones and count how many entries in the map are >=2
 day05a :: [Line] :~> Int
 day05a = MkSol
     { sParse = parseMaybe (some lineP)
@@ -76,6 +86,7 @@ day05a = MkSol
     , sSolve = Just . length . (filter (>= 2)) . concat . drawMap . (filter isStraight)
     }
 
+-- Draw the map and count how many entries in the map are >=2
 day05b :: [Line] :~> Int
 day05b = MkSol
     { sParse = parseMaybe (some lineP)
