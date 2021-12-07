e4ffd4f84ade2ae9962b31cdb5c625e07c183649
Carl Richard Theodor Schneider <dev.github@crtified.me>
2021-12-07 12:57:00 +0100

    Day 7 Solution

--- src/AOC/Challenge/Day07.hs
+++ src/AOC/Challenge/Day07.hs
@@ -1,6 +1,3 @@
-{-# OPTIONS_GHC -Wno-unused-imports   #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 -- |
 -- Module      : AOC.Challenge.Day07
 -- License     : BSD3
@@ -9,35 +6,51 @@
 -- Portability : non-portable
 --
 -- Day 7.  See "AOC.Solver" for the types used in this module!
---
--- After completing the challenge, it is recommended to:
---
--- *   Replace "AOC.Prelude" imports to specific modules (with explicit
---     imports) for readability.
--- *   Remove the @-Wno-unused-imports@ and @-Wno-unused-top-binds@
---     pragmas.
--- *   Replace the partial type signatures underscores in the solution
---     types @_ :~> _@ with the actual types of inputs and outputs of the
---     solution.  You can delete the type signatures completely and GHC
---     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day07 (
-    -- day07a
-  -- , day07b
+     day07a
+   , day07b
   ) where
 
-import           AOC.Prelude
 
-day07a :: _ :~> _
+import AOC.Solver ((:~>)(..))
+import Data.Void
+import Text.Megaparsec
+import Text.Megaparsec.Char (char, digitChar)
+import Control.Arrow ((&&&))
+import Data.Ix (range)
+
+type Crab = Int
+
+type Parser = Parsec Void String
+inputP :: Parser [Crab]
+inputP = (read <$> some digitChar) `sepBy1` (char ',')
+
+listBounds :: Ord a => [a] -> (a, a)
+listBounds = minimum &&& maximum
+
+linearFuelCost :: Int -> Crab -> Int
+linearFuelCost p c = abs $ c - p
+
+incrFuelCost :: Int -> Crab -> Int
+incrFuelCost p c = (n * (n+1)) `div` 2 -- Little Gauß
+  where
+    n = abs $ c - p
+
+totalFuel :: (Int -> Crab -> Int) -> [Crab] -> Int -> Int
+totalFuel f cs p = sum $ map (f p) cs
+
+day07a :: [Crab] :~> Int
 day07a = MkSol
-    { sParse = Just
+    { sParse = parseMaybe inputP
     , sShow  = show
-    , sSolve = Just
+    , sSolve = \input -> Just $ minimum $ map (totalFuel (linearFuelCost) input) $ range $ listBounds input
     }
 
-day07b :: _ :~> _
+day07b :: [Crab] :~> Int
 day07b = MkSol
-    { sParse = Just
+    { sParse = parseMaybe inputP
     , sShow  = show
-    , sSolve = Just
+    , sSolve = \input -> Just $ minimum $ map (totalFuel (incrFuelCost) input) $ range $ listBounds input
     }
+
