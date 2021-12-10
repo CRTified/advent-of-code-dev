2eba50871dc3b06f61c1d8aa77c3ca7053eba9cc
Carl Richard Theodor Schneider <dev.github@crtified.me>
2021-12-10 20:08:54 +0100

    Day 10 Solution

--- src/AOC/Challenge/Day10.hs
+++ src/AOC/Challenge/Day10.hs
@@ -22,22 +22,82 @@
 --     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day10 (
-    -- day10a
-  -- , day10b
+     day10a
+   , day10b
   ) where
 
-import           AOC.Prelude
+import AOC.Prelude
+import AOC.Solver ((:~>)(..))
+import Data.Void
+import Text.Megaparsec
+import Text.Megaparsec.Char (digitChar, space)
+import Data.Stack
+import Data.Maybe (catMaybes)
+import Data.List (sort)
 
-day10a :: _ :~> _
+stackToList :: Stack Char -> [Char]
+stackToList s
+  | stackIsEmpty s = []
+  | otherwise = v : (stackToList newS)
+  where
+    (newS, v) = case stackPop s of
+                Nothing -> (stackNew, ' ')
+                Just (s', v') -> (s', v')
+
+illegalScore :: Char -> Int
+illegalScore ')' = 3
+illegalScore ']' = 57
+illegalScore '}' = 1197
+illegalScore '>' = 25137
+illegalScore _ = 0
+
+completeScore :: Char -> Int
+completeScore '(' = 1
+completeScore '[' = 2
+completeScore '{' = 3
+completeScore '<' = 4
+completeScore _ = 0
+
+expected :: Char -> Char
+expected '(' = ')'
+expected '[' = ']'
+expected '{' = '}'
+expected '<' = '>'
+expected n = n
+
+score :: String -> (Maybe Int, Maybe Int)
+score = runStack stackNew
+  where
+    runStack acc [] = (Nothing, Just $ scoreStack acc)
+    runStack acc ('(':cs) = runStack (stackPush acc '(') cs
+    runStack acc ('[':cs) = runStack (stackPush acc '[') cs
+    runStack acc ('{':cs) = runStack (stackPush acc '{') cs
+    runStack acc ('<':cs) = runStack (stackPush acc '<') cs
+    runStack acc (c:cs)
+      | c == expected v = runStack nxt cs
+      | otherwise = (Just $ illegalScore c, Nothing)
+      where
+        (nxt, v) = case stackPop acc of
+                     Nothing -> (stackNew, ' ')
+                     Just (s, val) -> (s, val)
+    scoreStack :: Stack Char -> Int
+    scoreStack s = foldl (\acc v -> acc * 5 + completeScore v) 0 $ stackToList s
+    
+
+day10a :: [String] :~> Int
 day10a = MkSol
-    { sParse = Just
+    { sParse = Just . lines
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . sum . catMaybes . (map (fst . score))
     }
 
-day10b :: _ :~> _
+day10b :: [String] :~> Int
 day10b = MkSol
-    { sParse = Just
+    { sParse = Just . lines
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . medianEntry . sort . catMaybes . (map (snd . score))
     }
+  where
+    medianEntry :: [a] -> a
+    medianEntry xs = xs !! (((length xs) - 1) `div` 2)
+  
