65340b6ece96ca03be6e11958d4d3b61b78edaa6
Carl Richard Theodor Schneider <dev.github@crtified.me>
2021-12-11 21:24:37 +0100

    Day 11 Solution

--- src/AOC/Challenge/Day11.hs
+++ src/AOC/Challenge/Day11.hs
@@ -1,6 +1,3 @@
-{-# OPTIONS_GHC -Wno-unused-imports   #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 -- |
 -- Module      : AOC.Challenge.Day11
 -- License     : BSD3
@@ -22,22 +19,76 @@
 --     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day11 (
-    -- day11a
-  -- , day11b
+     day11a
+   , day11b
   ) where
 
-import           AOC.Prelude
+import AOC.Solver ((:~>)(..))
+import Data.Void
+import Text.Megaparsec
+import Text.Megaparsec.Char (digitChar, space)
+import Data.Map (Map)
+import qualified Data.Map as M
+import Control.Monad.State.Lazy as ST
 
-day11a :: _ :~> _
+type Parser = Parsec Void String
+
+type Coordinate = (Int, Int)
+data Octopus = Energy Int | Flashed
+type Game = Map Coordinate Octopus
+
+rowP :: Parser [Octopus]
+rowP = some $ (Energy . read) <$> count 1 digitChar
+
+inputP :: Parser Game
+inputP = do
+  rows <- rowP `sepBy` space
+  return $ M.fromList $ concat [ map (\(x, h) -> ((y, x), h)) rs | (y, rs) <- zip [0..] $ map (zip [0..]) rows]
+
+getNeighbours :: Coordinate -> [Coordinate]
+getNeighbours (x, y) = [ (x + dx, y + dy) | dx <- [-1..1], dy <- [-1..1]]
+
+addEnergy :: Octopus -> Octopus
+addEnergy (Energy e) = Energy $ e + 1
+addEnergy Flashed = Flashed
+
+resetEnergy :: Octopus -> Octopus
+resetEnergy Flashed = Energy 0
+resetEnergy n = n
+
+withEnergy :: (Int -> Bool) -> Octopus -> Bool
+withEnergy _ Flashed = False
+withEnergy f (Energy e) = f e
+
+step :: ST.State Game Int
+step = do
+  modify $ M.map (addEnergy)
+  n <- flash
+  modify $ M.map (resetEnergy)
+  return n
+
+flash :: ST.State Game Int
+flash = do
+  toFlash <- gets $ M.filter (withEnergy $ (<) 9)
+  if M.null toFlash then do
+    return 0
+    else do
+    let neighbours = (concat $ map (getNeighbours) $ M.keys toFlash)
+    modify $ M.union (M.map (const Flashed) toFlash)
+    modify $ \x -> foldl (\acc k -> M.alter (fmap addEnergy) k acc) x neighbours
+    newFlashes <- flash
+    return $ (M.size toFlash) + newFlashes
+   
+day11a :: Game :~> Int
 day11a = MkSol
-    { sParse = Just
+    { sParse = parseMaybe inputP
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . sum . (evalState (replicateM 100 step))
     }
 
-day11b :: _ :~> _
+day11b :: Game :~> Int
 day11b = MkSol
-    { sParse = Just
+    { sParse = parseMaybe inputP
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . (\(i, _) -> i +1) . last . (takeWhile (((/=) 100) . snd)) . (zip ([1..])) . (evalState (sequence $ repeat step))
     }
