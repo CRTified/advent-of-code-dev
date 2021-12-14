56b9202f36a64785ba19301570bb972b5e6bd199
Carl Richard Theodor Schneider <dev.github@crtified.me>
2021-12-14 01:10:26 +0100

    Day 13 Solution

--- src/AOC/Challenge/Day13.hs
+++ src/AOC/Challenge/Day13.hs
@@ -1,6 +1,3 @@
-{-# OPTIONS_GHC -Wno-unused-imports   #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 -- |
 -- Module      : AOC.Challenge.Day13
 -- License     : BSD3
@@ -22,22 +19,86 @@
 --     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day13 (
-    -- day13a
-  -- , day13b
+  day13a
+  , day13b
   ) where
 
-import           AOC.Prelude
+import AOC.Solver ((:~>)(..))
+import Data.Void
+import Text.Megaparsec
+import Text.Megaparsec.Char (string, eol, digitChar, char, space)
+import Data.Map (Map)
+import qualified Data.Map as M
+import Control.Arrow ((&&&))
 
-day13a :: _ :~> _
+type Coordinate = (Int, Int)
+type Paper = Map Coordinate Int
+
+type Parser = Parsec Void String
+data Fold = FoldX Int | FoldY Int
+
+-- A dot is x,y on a separate line
+dotP :: Parser Coordinate
+dotP = do
+  x <- read <$> some digitChar
+  char ','
+  y <- read <$> some digitChar
+  eol
+  return (x, y)
+
+-- Folds start with "fold along " and give the coordinate as [xy]=\d*
+foldP :: Parser Fold
+foldP = do
+  _ <- string "fold along "
+  choice [ string "x=" >> read <$> some digitChar >>= return . FoldX
+         , string "y=" >> read <$> some digitChar >>= return . FoldY
+         ]
+
+-- Input is some dots, an empty line, some folds
+inputP :: Parser (Paper, [Fold])
+inputP = do
+  dots <- some dotP
+  space
+  folds <- foldP `sepBy` space
+  return (M.fromList $ map (\(x, y) -> ((x,y), 1)) dots, folds)
+
+
+splitAlong :: Fold -> Coordinate -> Int -> Bool
+splitAlong (FoldX xline) (x, _) _ = x < xline
+splitAlong (FoldY yline) (_, y) _ = y < yline
+
+-- Apply a fold by flipping the coordinates along the fold line
+applyFold :: Paper -> Fold -> Paper
+applyFold p f = case f of
+  FoldX xl -> combine under (M.mapKeys (\(x, y) -> (2 * xl - x, y)) over)
+  FoldY yl -> combine under (M.mapKeys (\(x, y) -> (x, 2 * yl - y)) over)
+  where
+    (under, over) = M.partitionWithKey (splitAlong f) p
+    combine = M.unionWith (+)
+
+-- Show the coordinate map by getting the bounding box and mapping each value to a pixel
+showMap :: Map Coordinate a -> String
+showMap m = '\n': unlines [ map (\x -> getPixel x y) [minX..maxX] | y <- [minY..maxY]]
+  where
+    (minX, maxX) = (minimum &&& maximum) $ map (fst) $ M.keys m 
+    (minY, maxY) = (minimum &&& maximum) $ map (snd) $ M.keys m 
+    getPixel :: Int -> Int -> Char
+    getPixel x y
+      | isMember = '█'
+      | otherwise = ' '
+      where
+        isMember = M.member (x, y) m
+
+day13a :: (Paper, [Fold]) :~> Int
 day13a = MkSol
-    { sParse = Just
+    { sParse = parseMaybe inputP
     , sShow  = show
-    , sSolve = Just
+    , sSolve = \(paper, folds) -> Just $ length $ applyFold paper (head folds)
     }
 
-day13b :: _ :~> _
+day13b :: (Paper, [Fold]) :~> Paper
 day13b = MkSol
-    { sParse = Just
-    , sShow  = show
-    , sSolve = Just
+    { sParse = parseMaybe inputP
+    , sShow  = showMap
+    , sSolve = \(paper, folds) -> Just $ foldl (applyFold) paper folds
     }
