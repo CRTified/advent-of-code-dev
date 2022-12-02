14d1ff864bb245f3f9fa30f3bf9ab6af9f90141c
Carl Richard Theodor Schneider <dev.github@crtified.me>
2022-12-02 10:29:34 +0100

    day 2: Add solution

--- src/AOC/Challenge/Day02.hs
+++ src/AOC/Challenge/Day02.hs
@@ -1,6 +1,3 @@
-{-# OPTIONS_GHC -Wno-unused-imports   #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 -- |
 -- Module      : AOC.Challenge.Day02
 -- License     : BSD3
@@ -22,22 +19,120 @@
 --     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day02 (
-    -- day02a
-  -- , day02b
+     day02a
+   , day02b
   ) where
 
-import           AOC.Prelude
+import AOC.Solver ((:~>)(..))
+import Data.Void
+import Text.Megaparsec
+import Text.Megaparsec.Char (space, hspace)
 
-day02a :: _ :~> _
+type Parser = Parsec Void String
+
+data Move = Rock | Paper | Scissors deriving stock (Eq, Show)
+data Outcome = Loss | Draw | Win deriving stock (Eq, Show)
+type Strategy = (Move, Move)
+type StrategyGuide = [Strategy]
+
+type Strategy2 = (Move, Outcome)
+type StrategyGuide2 = [Strategy2]
+
+type Score = Int
+
+moveP :: Parser Move
+moveP = do
+  c <- oneOf "ABC"
+  return case c of
+              'A' -> Rock
+              'B' -> Paper
+              'C' -> Scissors
+              _ -> error "Invalid Parse"
+
+responseP :: Parser Move
+responseP =  do
+  c <- oneOf "XYZ"
+  return case c of
+              'X' -> Rock
+              'Y' -> Paper
+              'Z' -> Scissors
+              _ -> error "Invalid Parse"
+
+outcomeP :: Parser Outcome
+outcomeP =  do
+  c <- oneOf "XYZ"
+  return case c of
+              'X' -> Loss
+              'Y' -> Draw
+              'Z' -> Win
+              _ -> error "Invalid Parse"
+
+strategyP :: Parser Strategy
+strategyP = do
+  m <- moveP
+  hspace
+  r <- responseP
+  return (m, r)
+
+inputP :: Parser StrategyGuide
+inputP = strategyP `sepEndBy` space
+
+strategy2P :: Parser Strategy2
+strategy2P = do
+  m <- moveP
+  hspace
+  o <- outcomeP
+  return (m, o)
+
+input2P :: Parser StrategyGuide2
+input2P = strategy2P `sepEndBy` space
+
+moveScore :: Move -> Score
+moveScore Rock = 1
+moveScore Paper = 2
+moveScore Scissors = 3
+
+outcomeScore :: Outcome -> Score
+outcomeScore Loss = 0
+outcomeScore Draw = 3
+outcomeScore Win = 6
+
+gameScore :: Strategy -> Score
+gameScore (m, r) = (moveScore r) + outcomeScore outcome
+  where
+    outcome :: Outcome
+    outcome
+      | m == r = Draw
+      | (m == Scissors) && (r == Paper) = Loss
+      | (m == Rock) && (r == Scissors) = Loss
+      | (m == Paper) && (r == Rock) = Loss
+      | True = Win
+
+gameScore2 :: Strategy2 -> Score
+gameScore2 (m, o) = (moveScore response) + outcomeScore o
+  where
+    response :: Move
+    response
+      | o == Draw = m
+      | (o == Loss) && (m == Scissors) = Paper
+      | (o == Loss) && (m == Rock)     = Scissors
+      | (o == Loss) && (m == Paper)    = Rock
+      | (o ==  Win) && (m == Scissors) = Rock
+      | (o ==  Win) && (m == Rock)     = Paper
+      | (o ==  Win) && (m == Paper)    = Scissors
+      | True = error "What?"
+
+      
+day02a :: StrategyGuide :~> Score
 day02a = MkSol
-    { sParse = Just
+    { sParse = parseMaybe inputP
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . sum . (map gameScore)
     }
 
-day02b :: _ :~> _
+day02b :: StrategyGuide2 :~> Score
 day02b = MkSol
-    { sParse = Just
+    { sParse = parseMaybe input2P
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . sum . (map gameScore2)
     }
